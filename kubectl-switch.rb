fbb9990d0c8b880379545ae0e2973f11cdacd2e5
yulibaozi <yulibaozi@6617.com>
2019-05-12 18:26:54 +0800

    update kubectl-version

--- kubectl-switch.rb
+++ kubectl-switch.rb
@@ -1,7 +1,7 @@
 class KubectlSwitch < Formula
   desc "Kubernetes multi-cluster command-line management tool"
   homepage "https://github.com/yulibaozi/kubectl-switch"
-  url "https://github.com/yulibaozi/kubectl-switch/releases/download/v1.0.0/kubectl-switch_1.0.0_darwin_amd64.tar.gz"
+  url "https://github.com/yulibaozi/kubectl-switch/releases/download/v1.0.1/kubectl-switch_1.0.1_darwin_amd64.tar.gz"
   version "1.0.0"
   sha256 "854f953c96418ea516a6264d8408e59c814c54615b2a40ea25f32c05003ab314"
   head "https://github.com/yulibaozi/kubectl-switch.git"
